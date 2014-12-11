7730f8c67c4dc6848f52241d9e28496d2eea9962
Jacob Faulkner <Jakers@Jacobs-MacBook-Pro.local>
2014-12-10 20:42:54 -0700

    adding modal images, map, fixed route issues, and some style

--- www/js/services.js
+++ www/js/services.js
@@ -3,24 +3,24 @@ angular.module('starter.services', [])
 /**
  * A simple example service that returns some data.
  */
-.factory('Friends', function() {
-  // Might use a resource here that returns a JSON array
 
-  // Some fake testing data
-  var friends = [
-    { id: 0, name: 'Scruff McGruff' },
-    { id: 1, name: 'G.I. Joe' },
-    { id: 2, name: 'Miss Frizzle' },
-    { id: 3, name: 'Ash Ketchum' }
-  ];
+    .factory('Places', function($http) {
+        // Might use a resource here that returns a JSON array
 
-  return {
-    all: function() {
-      return friends;
-    },
-    get: function(friendId) {
-      // Simple index lookup
-      return friends[friendId];
-    }
-  }
-});
+        // Adding http request to load json file
+        var places = [];
+        $http.get('Locations/places.json').success(function(data){
+            places = data;
+        });
+
+        return {
+            all: function() {
+                return places;
+            },
+            get: function(locationId) {
+                // Simple index lookup
+                return places[locationId];
+            }
+        }
+    });
+
